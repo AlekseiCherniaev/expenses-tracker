482ff418d2339c2e8c86d68c016b904a9815561c
Aleksey Cherniaev <cherniaevaleksey2304@gmail.com>
2025-09-07 00:30:59 +0300

    feat: add user DTO

---
+++ expenses_tracker/application/dto/user.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+from datetime import datetime
+from uuid import UUID
+
+
+@dataclass
+class UserDTO:
+    id: UUID
+    username: str
+    email: str | None
+    is_active: bool
+    created_at: datetime
+    updated_at: datetime
+
+
+@dataclass
+class UserCreateDTO:
+    username: str
+    email: str | None
+    password: str
+
+
+@dataclass
+class UserUpdateDTO:
+    id: UUID
+    email: str | None
+    password: str | None
