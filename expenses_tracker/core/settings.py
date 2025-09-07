7fbd0271211890a02d84a9f13eac850ec5474fd2
Aleksey Cherniaev <cherniaevaleksey2304@gmail.com>
2025-09-07 13:09:15 +0300

    feat: add env path in settings

--- expenses_tracker/core/settings.py
+++ expenses_tracker/core/settings.py
@@ -1,10 +1,19 @@
+from pathlib import Path
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+env_dir = Path(__file__).parent.parent.parent
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(case_sensitive=False, frozen=True)
+    model_config = SettingsConfigDict(
+        case_sensitive=False,
+        frozen=True,
+        env_file=env_dir / ".env",
+        env_file_encoding="utf-8",
+    )
 
-    log_level: str = "INFO"
+    log_level: str = "DEBUG"
     fast_api_debug: bool = False
 
 
