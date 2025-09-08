74ec28fcf63e187fa3f4f788ec0d7645666ce72a
Aleksey Cherniaev <cherniaevaleksey2304@gmail.com>
2025-09-08 10:44:44 +0300

    feat: add user entity test

---
+++ tests/test_unit/entities/test_user.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from uuid import UUID
+
+
+from expenses_tracker.domain.entities.user import User
+
+
+class TestUser:
+    def test_user_creation(self):
+        username = "testuser"
+        hashed_password = "hashedpassword"
+
+        user = User(username, hashed_password)
+        assert user.username == username
+        assert user.hashed_password == hashed_password
+        assert isinstance(user.id, UUID)
+        assert user.is_active is False
+        assert user.email is None
+        assert isinstance(user.created_at, datetime)
+        assert isinstance(user.updated_at, datetime)
