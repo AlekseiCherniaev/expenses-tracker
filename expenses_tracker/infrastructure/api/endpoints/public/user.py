26c5bf36554e7e0ae136d995aa11e739ad59d0fc
Aleksey Cherniaev <cherniaevaleksey2304@gmail.com>
2025-09-10 08:16:55 +0300

    chore(logs): add additional debug logging

--- expenses_tracker/infrastructure/api/endpoints/public/user.py
+++ expenses_tracker/infrastructure/api/endpoints/public/user.py
@@ -25,6 +25,7 @@ async def get_user(user_id: UUID, request: Request) -> UserResponse:
         user_use_cases: UserUseCases = request.state.user_use_cases
         user_dto = await user_use_cases.get_user(user_id=user_id)
     except UserNotFound as e:
+        logger.bind(user_id=user_id).exception("User not found")
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
     logger.bind(user=user_dto).debug("Got user")
     return UserResponse(**user_dto.__dict__)
@@ -42,6 +43,7 @@ async def create_user(user_data: UserCreateRequest, request: Request) -> UserRes
         )
         user_dto = await user_use_cases.create_user(user_data=create_user_dto)
     except UserAlreadyExists as e:
+        logger.bind(user_data=user_data).exception("User already exists")
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=str(e))
     logger.bind(user=user_dto).debug("Created user")
     return UserResponse(**user_dto.__dict__)
@@ -60,8 +62,10 @@ async def update_user(user_data: UserUpdateRequest, request: Request) -> UserRes
         )
         user_dto = await user_use_cases.update_user(user_data=update_user_dto)
     except UserNotFound as e:
+        logger.bind(user_id=user_data.id).exception("User not found")
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
     except UserAlreadyExists as e:
+        logger.bind(user_id=user_data.id).exception("User already exists")
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=str(e))
     logger.bind(user=user_dto).debug("Updated user")
     return UserResponse(**user_dto.__dict__)
@@ -74,6 +78,7 @@ async def delete_user(user_id: UUID, request: Request) -> None:
         user_use_cases: UserUseCases = request.state.user_use_cases
         await user_use_cases.delete_user(user_id=user_id)
     except UserNotFound as e:
+        logger.bind(user_id=user_id).exception("User not found")
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
     logger.bind(user_id=user_id).debug("Deleted user")
     return None
