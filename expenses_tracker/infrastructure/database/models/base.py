6f26056afa44c0b16de5e248e8b65236b21db77d
Aleksey Cherniaev <cherniaevaleksey2304@gmail.com>
2025-09-11 10:00:57 +0300

    feat: add naming conventions in base model

--- expenses_tracker/infrastructure/database/models/base.py
+++ expenses_tracker/infrastructure/database/models/base.py
@@ -1,12 +1,22 @@
 from datetime import datetime
 from uuid import UUID, uuid4
 
-from sqlalchemy import func, DateTime
+from sqlalchemy import func, DateTime, MetaData
 from sqlalchemy.orm import DeclarativeBase, declared_attr, Mapped, mapped_column
+
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+metadata = MetaData(naming_convention=convention)
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
+    metadata = MetaData(naming_convention=convention)
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
