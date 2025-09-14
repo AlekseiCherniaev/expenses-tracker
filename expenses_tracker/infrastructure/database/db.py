aff5e90d91de41817befff50cb1e9d210f60ac00
Aleksey Cherniaev <cherniaevaleksey2304@gmail.com>
2025-09-14 23:24:25 +0300

    refactor: wrap async engine and session in DBEngine model

--- expenses_tracker/infrastructure/database/db.py
+++ expenses_tracker/infrastructure/database/db.py
@@ -1,16 +1,35 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from typing import Callable
+
+from pydantic import BaseModel
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker,
+    AsyncSession,
+    AsyncEngine,
+)
 
 from expenses_tracker.core.settings import settings
 
-engine = create_async_engine(
-    settings.async_postgres_url,
-    echo=settings.database_echo,
-    echo_pool=settings.database_pool_echo,
-    pool_size=settings.pool_size,
-)
 
-async_session_factory = async_sessionmaker(
-    bind=engine,
-    expire_on_commit=False,
-    class_=AsyncSession,
-)
+class DBEngine(BaseModel, arbitrary_types_allowed=True):
+    async_engine: AsyncEngine
+    async_session_factory: Callable[[], AsyncSession]
+
+
+def get_db_engines() -> DBEngine:
+    async_engine = create_async_engine(
+        settings.async_postgres_url,
+        echo=settings.database_echo,
+        echo_pool=settings.database_pool_echo,
+        pool_size=settings.pool_size,
+    )
+    async_session_factory = async_sessionmaker(
+        bind=async_engine,
+        expire_on_commit=False,
+        class_=AsyncSession,
+    )
+
+    return DBEngine(
+        async_session_factory=async_session_factory,
+        async_engine=async_engine,
+    )
