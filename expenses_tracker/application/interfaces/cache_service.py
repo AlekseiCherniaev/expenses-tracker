abc428bba3f4f95d91b92cac55e234b9fc441ce2
Aleksey Cherniaev <cherniaevaleksey2304@gmail.com>
2025-09-21 11:52:25 +0300

    feat(cache): update cache service methods to be asynchronous

--- expenses_tracker/application/interfaces/cache_service.py
+++ expenses_tracker/application/interfaces/cache_service.py
@@ -6,13 +6,13 @@ T = TypeVar("T")
 
 class ICacheService(ABC, Generic[T]):
     @abstractmethod
-    def get(self, key: str) -> T | None:
+    async def get(self, key: str) -> T | None:
         pass
 
     @abstractmethod
-    def set(self, key: str, value: T, ttl: int | None = None) -> None:
+    async def set(self, key: str, value: T, ttl: int | None = None) -> None:
         pass
 
     @abstractmethod
-    def delete(self, key: str) -> None:
+    async def delete(self, key: str) -> None:
         pass
