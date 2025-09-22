da6857b4354a4f6349df6a59418f8d8db12a43a4
Aleksey Cherniaev <cherniaevaleksey2304@gmail.com>
2025-09-23 00:17:45 +0300

    feat(auth): validate token type for access token authentication

--- expenses_tracker/infrastructure/api/dependencies/auth.py
+++ expenses_tracker/infrastructure/api/dependencies/auth.py
@@ -4,6 +4,8 @@ from fastapi import Depends
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 
 from expenses_tracker.application.interfaces.token_service import ITokenService
+from expenses_tracker.core.constants import TokenType
+from expenses_tracker.domain.exceptions.auth import InvalidCredentials
 from expenses_tracker.infrastructure.di import get_token_service
 
 bearer_scheme = HTTPBearer()
@@ -14,4 +16,6 @@ async def get_current_user_id(
     token_service: ITokenService = Depends(get_token_service),
 ) -> UUID:
     payload = token_service.decode_token(token=token.credentials)
+    if payload.type != TokenType.ACCESS:
+        raise InvalidCredentials("Provided token is not an access token")
     return UUID(payload.sub)
